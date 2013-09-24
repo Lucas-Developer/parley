f67e4388f81e6f7fcc29966a53d28d99bce153f6
David <david@david-AO521.(none)>
2013-09-24 13:00:31 -0500

    fuck

--- client/lib/parley.utility.js
+++ client/lib/parley.utility.js
@@ -135,12 +135,14 @@ ne
 
           zlib.inflateRaw(compressedBuffer, function (err, buffer) {
             if (err) { console.log(err); }
-            var data = buffer.toString();
+            else {
+              var data = buffer.toString();
 
-            //in the test case, the decompressed data appeared to have nonsense
-            //bits prepended to it.
-            //For our own purposes, the following fix is sufficient:
-            callback(JSON.parse(data.substr(data.indexOf('{"public"'))));
+              //in the test case, the decompressed data appeared to have nonsense
+              //bytes prepended to it.
+              //For our own purposes, the following fix is sufficient:
+              callback(JSON.parse(data.substr(data.indexOf('{"public"'))));
+            }
           });
         }
       }
